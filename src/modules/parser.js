bf3f3b53e27b30e570e4c0479df61f11cf97ec88
Waidhoferj <jwaidhof@calpoly.edu>
2019-10-26 21:17:57 -0700

    Added parsers

---
+++ src/modules/parser.js
@@ -0,0 +1,51 @@
+//TODO: add short words
+export function parseNameFrom(key) {
+  let isUppercase = char => char.charCodeAt(0) > 64 && char.charCodeAt(0) < 90;
+  let isCPE = (word, i) => word.slice(i, i + 3).toLowerCase() === "cpe";
+
+  let str = "";
+  for (let i = 0; i < key.length; i++) {
+    if (isCPE(key, i)) {
+      str += " CPE";
+      i += 2;
+    } else if (i === 0) str += key[i].toUpperCase();
+    else if (isUppercase(key[i])) str += " " + key[i];
+    else str += key[i];
+  }
+  return str;
+}
+
+export function parseKeyFrom(name) {
+  return name
+    .toLowerCase()
+    .split(" ")
+    .map((word, i) => (i === 0 ? word : word[0].toUpperCase() + word.slice(1)))
+    .join("");
+}
+
+export function parseCSV(csv) {
+  let nonDigit = /\D/g;
+  let quotes = /"/g;
+  let spacers = /,|\n/g;
+  let toPhoneNumbers = (numbers, entry) => {
+    let digits = entry.replace(nonDigit, "");
+    let notValidNumber =
+      digits.length < 10 || (digits.length === 11 && digits[0] !== "1");
+    if (notValidNumber) return numbers;
+    if (digits.length === 10) digits = "1" + digits;
+    numbers.push(digits);
+    return numbers;
+  };
+  return csv
+    .replace(quotes, "")
+    .split(spacers)
+    .reduce(toPhoneNumbers, []);
+}
+
+export function formatPhoneNumber(number) {
+  let notPhoneNumber = parseInt(number) < Math.pow(10, 9);
+  if (notPhoneNumber) return number;
+  let str = String(number);
+  if (str.length === 11) str = str.slice(1);
+  return str.slice(0, 3) + "-" + str.slice(3, 6) + "-" + str.slice(6, 10);
+}

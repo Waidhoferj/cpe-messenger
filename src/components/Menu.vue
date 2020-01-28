afcd9eb2d5d38e3696d725b70f927a2fd5709562
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-27 20:46:13 -0800

    updated app

---
+++ src/components/Menu.vue
@@ -0,0 +1,92 @@
+<template>
+  <div
+    @mouseenter="menuOpen = true"
+    @mouseleave="menuOpen = false"
+    @click="menuOpen = !menuOpen"
+    :class="{open: menuOpen}"
+    class="menu"
+  >
+    <img src="./assets/logo.svg" class="logo" />
+    <router-link tag="div" to="/announcements" class="option">
+      <img src="./assets/announcement-icon.svg" alt="img" />
+      <h3 v-show="menuOpen" class="menu-desc">Announcements</h3>
+    </router-link>
+    <router-link tag="div" to="/conversations" class="option">
+      <img src="./assets/conversation-icon.svg" alt="img" />
+      <h3 class="menu-desc" v-show="menuOpen">Conversations</h3>
+    </router-link>
+    <router-link tag="div" to="/groups" class="option">
+      <img src="./assets/groups-icon.svg" alt="img" />
+      <h3 class="menu-desc" v-show="menuOpen">Groups</h3>
+    </router-link>
+    <router-link tag="div" to="/" class="option logout" @click="$store.commit('logOut')">
+      <img src="@/assets/back-icon.svg" alt="Log out" />
+      <h3 class="menu-desc" v-show="menuOpen" style="white-space: nowrap">Log out</h3>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          menuOpen: false;
+      }
+  }
+};
+</script>
+
+<style lang="scss">
+.menu {
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 90px;
+  height: 100%;
+  background: var(--dark);
+  padding: 0 10px;
+  transition: width 0.7s;
+  overflow: hidden;
+  .logo {
+    margin: 10px 0;
+  }
+  .option {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    cursor: pointer;
+    padding: 10px;
+    margin: 5px 0;
+    transition: opacity 0.7s;
+
+    img {
+      width: 50px;
+      padding: 0 10px;
+    }
+
+    &:hover > .menu-desc {
+      color: var(--accent);
+    }
+
+    .menu-desc {
+      margin: 0;
+      color: white;
+      padding-left: 10px;
+      font-size: 1.5rem;
+      text-align: left;
+      transition: color 0.5s;
+    }
+
+    &.logout {
+      position: absolute;
+      bottom: 20px;
+      left: 10px;
+    }
+  }
+
+  &.open {
+    width: 300px;
+  }
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="centered-page sign-up">
+    <div v-if="loading" class="loader"></div>
+    <div class="login-container">
+      <div class="icon" ref="icon">
+        <img src="@/assets/letter.svg" alt />
+      </div>
+      <div class="divider"></div>
+      <h1 class="title">CPE Messenger</h1>
+      <h3>{{signUpOpen ? 'Sign up' : "Login"}}</h3>
+      <form @submit.prevent="signUpOpen ? signUp() : logIn()" ref="form">
+        <input v-if="signUpOpen" type="text" placeholder="Name" v-model="name" />
+        <input type="text" class="username" v-model="username" placeholder="Email" />
+        <input type="password" class="password" v-model="password" placeholder="Password" />
+        <input v-if="signUpOpen" type="password" v-model="code" placeholder="Entry Code" />
+        <input type="submit" style="display: none" />
+      </form>
+      <button
+        class="button-primary"
+        @click="signUpOpen ? signUp() : logIn()"
+      >{{signUpOpen ? "Sign up" : "Login"}}</button>
+      <button
+        class="button-secondary"
+        @click="signUpOpen = !signUpOpen"
+      >{{signUpOpen ? 'Login' : "Sign up"}}</button>
+    </div>
+    <transition name="sign-up-confirmation-popup" mode="out-in">
+      <div v-if="popupShown" class="sign-up-confirmation-popup">
+        <h3>Thanks for signing up!</h3>
+        <p>Your account should be ready in a couple of minutes</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>

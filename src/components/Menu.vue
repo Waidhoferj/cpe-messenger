f35e2d41839bc13f8e527840f2e8b1f155407820
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-31 12:57:10 -0800

    Added responsive menu for mobile

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,35 +1,37 @@
 <template>
-  <div
-    class="menu"
-    @mouseenter="menuOpen = true"
-    @mouseleave="menuOpen = false"
-    @click="menuOpen = !menuOpen"
-    :class="{ open: menuOpen }"
-  >
-    <img src="@/assets/logo.svg" class="logo" />
-    <router-link tag="div" to="/announcements" class="option">
-      <img src="@/assets/announcement-icon.svg" alt="img" />
-      <h3 v-show="menuOpen" class="menu-desc">Announcements</h3>
-    </router-link>
-    <router-link tag="div" to="/conversations" class="option">
-      <img src="@/assets/conversation-icon.svg" alt="img" />
-      <h3 class="menu-desc" v-show="menuOpen">Conversations</h3>
-    </router-link>
-    <router-link tag="div" to="/groups" class="option">
-      <img src="@/assets/groups-icon.svg" alt="img" />
-      <h3 class="menu-desc" v-show="menuOpen">Groups</h3>
-    </router-link>
-    <router-link
-      tag="div"
-      to="/"
-      class="option logout"
-      @click="$store.commit('logOut')"
+  <div class="menu">
+    <img
+      class="hamburger-icon"
+      src="@/assets/hamburger.svg"
+      alt="menu"
+      @click="menuOpen = true"
+      :class="{hide: menuOpen}"
+    />
+    <div
+      class="sidebar"
+      :class="{ open: menuOpen }"
+      @mouseenter="menuOpen = true"
+      @mouseleave="menuOpen = false"
+      @click="menuOpen = !menuOpen"
     >
-      <img src="@/assets/back-icon.svg" alt="Log out" />
-      <h3 class="menu-desc" v-show="menuOpen" style="white-space: nowrap">
-        Log out
-      </h3>
-    </router-link>
+      <img src="@/assets/logo.svg" class="logo" />
+      <router-link tag="div" to="/announcements" class="option">
+        <img src="@/assets/announcement-icon.svg" alt="img" />
+        <h3 v-show="menuOpen" class="menu-desc">Announcements</h3>
+      </router-link>
+      <router-link tag="div" to="/conversations" class="option">
+        <img src="@/assets/conversation-icon.svg" alt="img" />
+        <h3 class="menu-desc" v-show="menuOpen">Conversations</h3>
+      </router-link>
+      <router-link tag="div" to="/groups" class="option">
+        <img src="@/assets/groups-icon.svg" alt="img" />
+        <h3 class="menu-desc" v-show="menuOpen">Groups</h3>
+      </router-link>
+      <router-link tag="div" to="/" class="option logout" @click="$store.commit('logOut')">
+        <img src="@/assets/back-icon.svg" alt="Log out" />
+        <h3 class="menu-desc" v-show="menuOpen" style="white-space: nowrap">Log out</h3>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -45,56 +47,88 @@ export default {
 
 <style lang="scss">
 .menu {
-  position: relative;
   z-index: 100;
   position: fixed;
   top: 0;
   left: 0;
-  width: 90px;
   height: 100vh;
-  background: var(--dark);
-  padding: 0 10px;
-  transition: width 0.7s;
-  overflow: hidden;
-  .logo {
-    margin: 10px 0;
-  }
-  .option {
-    display: flex;
-    align-items: center;
-    text-align: center;
+
+  .hamburger-icon {
+    position: fixed;
+    top: 15px;
+    left: 15px;
     cursor: pointer;
-    padding: 10px;
-    margin: 5px 0;
-    transition: opacity 0.7s;
+    width: 40px;
+    display: none;
 
-    img {
-      width: 50px;
-      padding: 0 10px;
-    }
-
-    &:hover > .menu-desc {
-      color: var(--accent);
-    }
-
-    .menu-desc {
-      margin: 0;
-      color: white;
-      padding-left: 10px;
-      font-size: 1.5rem;
-      text-align: left;
-      transition: color 0.5s;
-    }
-
-    &.logout {
-      position: absolute;
-      bottom: 20px;
-      left: 10px;
+    &.hide {
+      pointer-events: none;
     }
   }
+  .sidebar {
+    width: 90px;
+    height: 100%;
+    background: var(--dark);
+    padding: 0 10px;
+    transition: width 0.7s, opacity 0.7s;
+    overflow: hidden;
+    .logo {
+      margin: 10px 0;
+    }
+    .option {
+      display: flex;
+      align-items: center;
+      text-align: center;
+      cursor: pointer;
+      padding: 10px;
+      margin: 5px 0;
+      transition: opacity 0.7s;
 
-  &.open {
-    width: 300px;
+      img {
+        width: 50px;
+        padding: 0 10px;
+      }
+
+      &:hover > .menu-desc {
+        color: var(--accent);
+      }
+
+      .menu-desc {
+        margin: 0;
+        color: white;
+        padding-left: 10px;
+        font-size: 1.5rem;
+        text-align: left;
+        transition: color 0.5s;
+      }
+
+      &.logout {
+        position: absolute;
+        bottom: 20px;
+        left: 10px;
+      }
+    }
+
+    &.open {
+      width: 300px;
+    }
+  }
+}
+@media screen and (max-width: 500px) {
+  .menu {
+    .hamburger-icon {
+      display: block;
+    }
+    .sidebar {
+      opacity: 0;
+      pointer-events: none;
+
+      &.open {
+        width: 100vw;
+        opacity: 1;
+        pointer-events: all;
+      }
+    }
   }
 }
 </style>
